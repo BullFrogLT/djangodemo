9db97c1f2d76521c56ac93a6efb77bc01e8493f6
beinier1121@sina.com <beinier1121@sina.com>
2018-12-29 16:46:08 +0800

    update

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from models import AlertReport
@@ -15,4 +17,13 @@ def index(request):
 
 
 def search(request):
-    return render(request, "index1.html")
+    data = {}
+    # all count
+    data['result'] = AlertReport.objects.all()
+    # ip count
+    data['ipcount'] = data['result'].values('from_machine').distinct().order_by('from_machine').count()
+    #
+
+
+    return render(request, "search.html", {'data': data})
+
